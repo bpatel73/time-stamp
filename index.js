7ab10e2faca9b7b1f55336c7e4fb34d5b3d41265
Bhaumik <bhaumik.ptl86@gmail.com>
2024-07-09 15:50:42 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // index.js
 // where your node app starts
+console.log(new Date());
+console.log(new Date('2015-12-25T00:00:00.000Z') - new Date('1970-01-01T00:00:00.000Z'));
+console.log(new Date(1451001600000).toUTCString());
+console.log(new Date('2015-12-25') - new Date('1970-01-01'));
+console.log(new Date('1970-01-01') - new Date('1970-01-01'));
 
 // init project
 var express = require('express');
@@ -24,7 +29,30 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/:date?", function(req, res){
+  var epoch = new Date('1970-01-01');
+  let date;
+  if(!req.params.date){
+    date = new Date();
+  }else if(Number(req.params.date)){
+    date = new Date(Number(req.params.date));
+  }else{
+    date = new Date(req.params.date);
+  }
+  var unix = date - epoch;
+  
+  console.log(unix, date);
+  if(date.toString() === 'Invalid Date'){
+    res.json({
+      "error": "Invalid Date"
+    });
+  }else{
+    res.json({
+      "unix": unix,
+      "utc": date.toUTCString()
+    });
+  }
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
